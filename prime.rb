e9ff8617c739038bf9429ba525bf3b7633d1f6d8
Koray Ozkal <koryozkal@gmail.com>
2020-04-23 05:53:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 
   
   def prime?(number)
-  check = false	  check = false
-  count = 2	  count = 2
+  check = false	  
+  count = 2	  
 @@ -14,3 +15,20 @@ def prime?(number)
-  return check	  return check
+  return check	 
 end
 end
